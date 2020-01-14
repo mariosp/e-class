f0a14fa05c689c54a2ef49311f643009d124c1c3
Marios <mariospatsis@gmail.com>
2020-01-14 23:19:36 +0200

    comments

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken');
 const {User} = require("../models");
 
-// Router level middleware to Check the Jwt token if is valid
+/*
+ Router level middleware to Check the Jwt token if is valid
+*/
 const checkAuthToken = async (req, res, next)=> {
-    console.log(req.header('Authorization'));
     try {
-        const token = req.header('Authorization').replace('Bearer ', ''); //Remove the Bearer from the start of the token's string
+        const token = req.header('Authorization').replace('Bearer ', ''); //Remove the Bearer and the space from the start of the token's string
         const jwtObject = jwt.verify(token, process.env.JWT_SECRET);
         jwtObject ||  res.status(401).send({ status:0,msg: 'Your need to authenticate ' });
-        console.log(jwtObject)
         const user = await User.findOne({ _id: jwtObject._id, "accessTokens.token": token });
         user || res.status(401).send({ status:0,msg: 'Your need to authenticate ' });
         req.token = token;
@@ -19,11 +19,13 @@ const checkAuthToken = async (req, res, next)=> {
     }
 };
 
-
+/*
+ routePermission
+ Give access to the route based on the role of the user
+*/
 const routePermission =  (routePermissions) => {
     return  (req, res, next) => {
         const permitted = routePermissions.some((permission)=> req.user.userRole === permission);
-        console.log(permitted);
         permitted && next();
         permitted || res.status(401).send({error: "Not permitted"});
     }
