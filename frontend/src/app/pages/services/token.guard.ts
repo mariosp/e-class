e22365662d2f65e3f000d16fbec54712d4629ab7
Marios <mariospatsis@gmail.com>
2020-01-19 11:31:25 +0200

    title margin, delete specs

--- frontend/src/app/pages/services/token.guard.ts
+++ frontend/src/app/pages/services/token.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Routes, Router} from '@angular/router';
 import { Observable } from 'rxjs';
-import {JwtHelperService} from "@auth0/angular-jwt";
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: 'root'
@@ -14,9 +14,6 @@ export class TokenGuard implements CanActivate {
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if(localStorage.getItem("TOKEN")){
       const expired = new JwtHelperService().isTokenExpired(localStorage.getItem("TOKEN"));
-      console.log(expired)
-      //const decodedToken = this.jwtHelper.decodeToken(localStorage.getItem('TOKEN'));
-      // const expired = this.jwtHelper.isTokenExpired(localStorage.getItem("TOKEN"));
       if(!expired) return true;
       this.nav.navigate(['/login']);
       return false;
