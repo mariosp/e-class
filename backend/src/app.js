413ef6fa20bb85ac2a86f0f636540b02bce38d47
Marios <mariospatsis@gmail.com>
2020-01-20 03:44:01 +0200

    heroku deployment 9

--- backend/src/app.js
+++ backend/src/app.js
@@ -35,7 +35,7 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, "../../frontend/dist/frontend/index.html"));
 });
 
